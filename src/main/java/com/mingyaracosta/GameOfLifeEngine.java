47b26951eefc3cef248288236534a89fb91e221e
Mingyar Acosta <minyoach@gmail.com>
2019-08-20 23:50:31 -0400

    Added testShouldKeepAliveWith2AliveNeighbors, testShouldKeepAliveWith3AliveNeighbors, testItShouldDieBecausOfLonelinessWith1AliveNeighbor, testItShouldDieBecausOfLonelinessWithNoAliveNeighbor and testItShouldDieBecausOfOverpopulationWith5AliveNeighbor in GameOfLifeEngineTest

--- src/main/java/com/mingyaracosta/GameOfLifeEngine.java
+++ src/main/java/com/mingyaracosta/GameOfLifeEngine.java
@@ -1,4 +1,17 @@
 package com.mingyaracosta;
 
+import com.mingyaracosta.rules.AliveCellsRule;
+import com.mingyaracosta.rules.IRule;
+
 public class GameOfLifeEngine {
+
+    private IRule firstRule;
+
+    public GameOfLifeEngine() {
+        this.firstRule = new AliveCellsRule();
+    }
+
+    Cell getCellForNextGeneration(Cell currentCell, int aliveNeighborsCount) {
+        return this.firstRule.evaluate(currentCell, aliveNeighborsCount);
+    }
 }
--- src/test/java/com/mingyaracosta/GameOfLifeEngineTest.java
+++ src/test/java/com/mingyaracosta/GameOfLifeEngineTest.java
@@ -1,8 +1,56 @@
 package com.mingyaracosta;
 
+import com.mingyaracosta.rules.AliveCellsRule;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameOfLifeEngineTest {
-/*@Test
-    public void testShouldKeepA*/
+    private static Cell cell;
+    private static GameOfLifeEngine gameEngine;
+
+    @BeforeClass
+    public static void setUp() {
+        gameEngine = new GameOfLifeEngine();
+    }
+
+    @Test
+    public void testShouldKeepAliveWith2AliveNeighbors() {
+        cell = new Cell(State.ALIVE);
+        Cell possibleDeadCell = gameEngine.getCellForNextGeneration(cell, 2);
+        Assert.assertNotNull(possibleDeadCell);
+        Assert.assertEquals(State.ALIVE, possibleDeadCell.getState());
+    }
+
+    @Test
+    public void testShouldKeepAliveWith3AliveNeighbors() {
+        cell = new Cell(State.ALIVE);
+        Cell possibleDeadCell = gameEngine.getCellForNextGeneration(cell, 2);
+        Assert.assertNotNull(possibleDeadCell);
+        Assert.assertEquals(State.ALIVE, possibleDeadCell.getState());
+    }
+
+    @Test
+    public void testItShouldDieBecausOfLonelinessWith1AliveNeighbor() {
+        cell = new Cell(State.ALIVE);
+        Cell possibleDeadCell = gameEngine.getCellForNextGeneration(cell, 1);
+        Assert.assertNotNull(possibleDeadCell);
+        Assert.assertEquals(State.DEAD, possibleDeadCell.getState());
+    }
+
+    @Test
+    public void testItShouldDieBecausOfLonelinessWithNoAliveNeighbor() {
+        cell = new Cell(State.ALIVE);
+        Cell possibleDeadCell = gameEngine.getCellForNextGeneration(cell, 0);
+        Assert.assertNotNull(possibleDeadCell);
+        Assert.assertEquals(State.DEAD, possibleDeadCell.getState());
+    }
+
+    @Test
+    public void testItShouldDieBecausOfOverpopulationWith5AliveNeighbor() {
+        cell = new Cell(State.ALIVE);
+        Cell possibleDeadCell = gameEngine.getCellForNextGeneration(cell, 5);
+        Assert.assertNotNull(possibleDeadCell);
+        Assert.assertEquals(State.DEAD, possibleDeadCell.getState());
+    }
 }
